49278c159792fbf9ba44c4c1bf9bebb95b98144e
Daniel Roke <droque123@gmail.com>
2023-08-16 13:45:16 +0300

    Stylize metronome

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,6 +1,70 @@
 import React, {useState, useEffect, useRef} from 'react';
+import styled, { keyframes } from 'styled-components';
 
 const host = window.location.hostname === "localhost" ? window.location.origin : "https://dankocher.github.io/metronome_reactjs";
+
+const MetronomeWrapper = styled.div`
+  text-align: center;
+  position: relative;
+  width: 200px;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Controls = styled.div`
+  margin-top: 20px;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  
+`;
+
+const Count = styled.div`
+  font-size: 24px;
+  margin-top: 10px;
+`;
+
+const Button = styled.button`
+  font-size: 18px;
+  margin-top: 10px;
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const tickAnimation = (tempo) => keyframes`
+  0%, 100% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(120px);
+  }
+`;
+
+const BouncingCircle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: #ff5733;
+  border-radius: 50%;
+  position: absolute;
+  animation: ${(props) => tickAnimation(props.tempo)} ${(props) => 120 / props.tempo}s linear infinite;
+`;
+
 
 function Metronome() {
     const [tempo, setTempo] = useState(200);
@@ -8,47 +72,54 @@ function Metronome() {
     const [beats, setBeats] = useState(0);
 
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const tickSound = useRef(new Audio(host + "/audio/sound0.wav"));
+    const tickSound = new Audio(host + "/audio/sound0.wav");
 
     useEffect(() => {
         if (isPlaying) {
-            tick();
-            const timer = setInterval(tick, (60 / tempo) * 1000);
+            const timer = setInterval(() => {
+                tick();
+                setBeats((prevCount) => prevCount + 1);
+            }, (60 / tempo) * 1000);
             return () => clearInterval(timer);
         }
     }, [isPlaying, tempo]);
 
-    const handleTempoChange = (event) => {
-        setTempo(parseInt(event.target.value, 10) || 0);
-    }
+    const tick = () => {
+        tickSound.currentTime = 0;
+        tickSound.play();
+    };
 
     const toggleMetronome = () => {
-        setIsPlaying(!isPlaying);
-    }
-
-    const tick = () => {
-        tickSound.current.currentTime = 0;
-        tickSound.current.play();
-        setBeats(v => v+1)
-    }
+        if (isPlaying) {
+            setIsPlaying(false);
+            setBeats(0);
+        } else {
+            setIsPlaying(true);
+        }
+    };
 
     return (
-        <div className="metronome">
+        <MetronomeWrapper>
             <h1>Metronome</h1>
-            <h2>Beats: <b>{beats}</b></h2>
-            <div className="controls">
+            <Count>Tics: {beats}</Count>
+            <div style={{width: 150, height: 20}}>
+                <BouncingCircle tempo={tempo} style={{display: isPlaying ? "block" : "none"}}/>
+            </div>
+            <Controls>
                 <label htmlFor="tempo">Tempo: </label>
                 <input
                     type="number"
                     id="tempo"
                     value={tempo}
-                    onChange={handleTempoChange}
+                    style={{marginTop: 10, textAlign: "center", fontSize: 20, width: 80}}
+                    onChange={(e) => setTempo(parseInt(e.target.value, 10) || 0)}
+                    disabled={isPlaying}
                 />
-                <button onClick={toggleMetronome}>
+                <Button onClick={toggleMetronome}>
                     {isPlaying ? 'Stop' : 'Start'}
-                </button>
-            </div>
-        </div>
+                </Button>
+            </Controls>
+        </MetronomeWrapper>
     );
 }
 
